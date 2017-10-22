244770a18c7f88c746dcb350b7493fb906562db2
lidehua <lidehua307@163.com>
2017-10-22 21:44:23 +0800

    12

--- Angular书籍/angularJS/js/0725/内容和作业/zuoye1 js.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Administrator on 2016/7/25.
- */
-console.log("这是外部的js打印出来的")
--- Angular书籍/angularJS/js/0725/内容和作业/zuoye3.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Administrator on 2016/7/25.
- */
-document.write('<p>你很棒</p>')
-document.write('<h1>你很棒</h1>')
